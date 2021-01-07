c623ec6fe021667feec2e6702431678f8195b4b8
hamdidev <itechtranss77@gmail.com>
2021-01-07 16:03:54 +0300

    Final Project

---
+++ server/index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv  from "dotenv"
+dotenv.config();
+import postRoutes from './routes/posts.js'
+
+
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+
+//MiddleWares
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+
+// Connecting to db
+
+mongoose
+.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, ()=> console.log(`server running on port ${PORT} and DB Connected`)));
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+mongoose.set('useFindAndModify', false);
+
+
+
+// App Middlewares
+app.use('/posts', postRoutes)
+
+
+
+
+
+
+
+
